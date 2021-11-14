fa5cdfd14b69887c5cd55b273c6abd35bcbddb9c
abhira0 <abhishekr0@protonmail.com>
2021-11-15 01:02:50 +0530

    tmp

--- run.py
+++ run.py
@@ -1,4 +1,12 @@
-from dbdl.magoosh import MAG_D, MAG_H
+from dbdl.magoosh import MAG_D, MAG_H, MAG_IDM
 
-DOWNLOAD_PATH = "./downloads"
-MAG_H(DOWNLOAD_PATH, "info")
+DOWNLOAD_PATH = "F:\scr"
+
+
+def MAG_GRE():
+    # MAG_H(DOWNLOAD_PATH, "info")
+    MAG_D(DOWNLOAD_PATH, "info")
+    # MAG_IDM(DOWNLOAD_PATH, "info")
+
+
+MAG_GRE()
