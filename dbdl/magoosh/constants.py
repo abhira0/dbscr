6ef2e7a5a151404d0f0034c9868b051d79ca589a
abhira0 <abhishekr0@protonmail.com>
2021-11-14 20:59:08 +0530

    create magoosh/constants

---
+++ dbdl/magoosh/constants.py
@@ -0,0 +1,23 @@
+ROOT_URL = "https://gre.magoosh.com"
+BASE_URL = "https://gre.magoosh.com/lessons"
+
+BASE_U_PATH = "magoosh\\ultimatum.json"  # ultimatum
+BASE_D_PATH = "magoosh\\db"  # download database
+BASE_L_PATH = "magoosh\\log.json"  # log
+BASE_PATH = "magoosh"  # base
+
+COOKIES = ""  # TODO Add cookies HERE
+CONF = {"thread": {"lecture": 30, "download": 25}}
+
+CN = 105  # [i] normal-text -> purple-magenta
+CC = (78, 201, 176)  # Class name -> Greenish (vscode-clr)
+CU = "magenta"  # URL -> magenta
+CA = 99  # contrast of CB -> light magenta
+CB = 201  # contrast of CA -> pink
+CT = (167, 206, 155)  # time, numbers -> light green (vscode-clr)
+
+CS = (22, 198, 12)  # success -> green
+CW = (252, 225, 0)  # warning -> yellow
+CE = "red"  # error -> red
+
+TMP_I = ["i", CB, CA]
