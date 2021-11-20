575ea6eb5b6b7db104186b07b935429b4b5c9511
abhira0 <abhishekr0@protonmail.com>
2021-11-20 10:15:27 +0530

    NEWSITE: gre.magoosh

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ DOWNLOAD_PATH = "F:\scr"
 
 
 def MAG_GRE():
-    # MAG_H(DOWNLOAD_PATH, "info")
+    MAG_H(DOWNLOAD_PATH, "info")
     MAG_D(DOWNLOAD_PATH, "info")
     # MAG_IDM(DOWNLOAD_PATH, "info")
 
