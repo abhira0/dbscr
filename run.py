d0fc7b1d889f649eaae459546898a33c96d709da
abhira0 <abhishekr0@protonmail.com>
2021-11-20 13:35:59 +0530

    NEWSITE: scrolller.com

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 from dbdl.magoosh import MAG_D, MAG_H, MAG_IDM
+from dbdl.scrolller import SLR_D, SLR_H
 
 DOWNLOAD_PATH = "F:\scr"
 
@@ -9,4 +10,7 @@ def MAG_GRE():
     # MAG_IDM(DOWNLOAD_PATH, "info")
 
 
-MAG_GRE()
+def SLR():
+    sub_name = "ImaginaryCaves"
+    SLR_H(sub_name, DOWNLOAD_PATH, "info")
+    SLR_D(sub_name, DOWNLOAD_PATH, "info")
