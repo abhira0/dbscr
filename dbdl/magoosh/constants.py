3c628a97ed40ec0a0daf165fbb930c490fadb87f
abhira0 <abhishekr0@protonmail.com>
2021-11-20 10:10:27 +0530

    minor changes

--- dbdl/magoosh/constants.py
+++ dbdl/magoosh/constants.py
@@ -5,9 +5,12 @@ BASE_U_PATH = "magoosh\\ultimatum.json"  # ultimatum
 BASE_D_PATH = "magoosh\\db"  # download database
 BASE_L_PATH = "magoosh\\log.json"  # log
 BASE_PATH = "magoosh"  # base
+IDM_PATH = "C:\Program Files (x86)\Internet Download Manager\IDMan.exe"
+BATCH_PATH = "magoosh\\download.bat"
+
 
 COOKIES = ""  # TODO Add cookies HERE
-CONF = {"thread": {"lecture": 30, "download": 25}}
+CONF = {"thread": {"lecture": 1, "download": 10}}
 
 CN = 105  # [i] normal-text -> purple-magenta
 CC = (78, 201, 176)  # Class name -> Greenish (vscode-clr)
@@ -21,3 +24,4 @@ CW = (252, 225, 0)  # warning -> yellow
 CE = "red"  # error -> red
 
 TMP_I = ["i", CB, CA]
+TMP_E = ["!", CA, "red"]
