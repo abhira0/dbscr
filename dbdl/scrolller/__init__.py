cde2d4ede09c3bb93f5ae7e44cfc09825f71b286
abhira0 <abhishekr0@protonmail.com>
2021-12-16 20:22:24 +0530

    new CLI interface

--- dbdl/scrolller/__init__.py
+++ dbdl/scrolller/__init__.py
@@ -1,2 +1,3 @@
 from .downloader import Downloader as SLR_D
 from .harvester import Harvester as SLR_H
+from .dupy import Dupy as SLR_DUPY
--- run.py
+++ run.py
@@ -1,16 +1,69 @@
+from pyrhd.utility.cprint import printInfo
 from dbdl.magoosh import MAG_D, MAG_H, MAG_IDM
-from dbdl.scrolller import SLR_D, SLR_H
+from dbdl.magoosh.constants import CW, TMP_I
+from dbdl.scrolller import SLR_D, SLR_H, SLR_DUPY
+import argparse
 
 DOWNLOAD_PATH = "F:\scr"
 
 
-def MAG_GRE():
-    MAG_H(DOWNLOAD_PATH, "info")
-    MAG_D(DOWNLOAD_PATH, "info")
+def MAG_GRE(a):
+    MAG_H(DOWNLOAD_PATH, "info") if not a.noharvest else None
+    MAG_D(DOWNLOAD_PATH, "info") if a.download else None
     # MAG_IDM(DOWNLOAD_PATH, "info")
 
 
-def SLR():
-    sub_name = "ImaginaryCaves"
-    SLR_H(sub_name, DOWNLOAD_PATH, "info")
-    SLR_D(sub_name, DOWNLOAD_PATH, "info")
+def SLR(a):
+    sub_name = (a.q or input("Sub name: ")).strip()
+    SLR_H(sub_name, DOWNLOAD_PATH, "info") if not a.noharvest else None
+    SLR_D(sub_name, DOWNLOAD_PATH, "info") if a.download else None
+    # SLR_DUPY(DOWNLOAD_PATH)
+
+
+def argument_owner():
+    a = argparse.ArgumentParser()
+    a.add_argument(
+        "-s",
+        help="Website URL to scrape",
+        default=None,
+        nargs="?",
+    )
+    a.add_argument(
+        "-q",
+        help="Query to pass into the website (if any)",
+        default=None,
+        nargs="?",
+    )
+    a.add_argument(
+        "-d", "--download", help="Whether to download or not", action="store_true"
+    )
+    a.add_argument(
+        "-nh", "--noharvest", help="Do not need to harvest?", action="store_true"
+    )
+    a.add_argument(
+        "-dp",
+        help="Download Path",
+        default=None,
+        nargs="?",
+    )
+    return a
+
+
+ao = argument_owner()
+a = ao.parse_args()
+
+
+# total_files = len(list(Utils.os.getAllFiles(DOWNLOAD_PATH)))
+
+if a.dp:
+    DOWNLOAD_PATH = a.dp
+
+
+if not a.s:
+    ao.print_help()
+elif "scrolller.com" in a.s:
+    SLR(a)
+elif "gre.magoosh.com" in a.s:
+    MAG_GRE(a)
+else:
+    printInfo(*TMP_I, "Currently, given website is not supported by the program.", CW)
